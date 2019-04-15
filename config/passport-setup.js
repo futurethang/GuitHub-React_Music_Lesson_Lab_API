cd0db95725018eb11cbe0953f2727f404bb60b09
Kevin <kphyde@gmail.com>
2019-04-15 13:59:54 -0700

    update keys to live in dotenv, update beanstalk deployment to save securely

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,5 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./keys");
 const User = require("../models/user");
 
 passport.serializeUser((user, done) => {
@@ -17,8 +16,8 @@ passport.use(
   new GoogleStrategy(
     {
       // options for google strategy
-      clientID: keys.google.clientID,
-      clientSecret: keys.google.clientSecret,
+      clientID: process.env.GOOGLECLIENTID,
+      clientSecret: process.env.GOOGLECLIENTSECRET,
       callbackURL: "/auth/google/redirect"
     },
     (accessToken, refreshToken, profile, done) => {
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require("express");
+require('dotenv').config()
 const authRoutes = require("./routes/auth-routes");
 const profileRoutes = require("./routes/profile-routes");
 const passportSetup = require("./config/passport-setup"); // required for functions in passport-setup to initialize before use in this file
 const routes = require("./routes");
 const mongoose = require("mongoose");
-const keys = require("./config/keys");
 const cookieSession = require('cookie-session');
 const passport = require('passport')
 const morgan = require('morgan')
 const app = express();
-const PORT = process.env.PORT || 8081;
-require('dotenv').config()
+const PORT = process.env.PORT || 3001;
 
 // Define middleware
 app.use(morgan('dev'))
@@ -28,7 +27,7 @@ app.set("view engine", "ejs");
 
 app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000,
-  keys: [keys.session.cookieKey]
+  keys: [process.env.COOKIEKEY]
 }))
 
 // initialize passport
@@ -36,7 +35,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // connect to mongoDB
-mongoose.connect(keys.mongoDB.dbURI, () => {
+mongoose.connect(process.env.DBURI, () => {
   console.log('connected to mongodb')
 });
 
